8372710bea1595491a171a3297098fed8ee66205
Kalle Blomström <kalle.blomstrom@gmail.com>
2016-03-01 00:07:54 +0100

    adds ouat2-params map example for spotify

--- src/loudmoauth_examples/core.clj
+++ src/loudmoauth_examples/core.clj
@@ -3,11 +3,17 @@
             [ring.adapter.jetty :as ringj]))
 
 (defn handler [request]
-  (condp (:uri request)
+  (condp = (:uri request)
      "/oauth2" (lm/parse-code request) 
     {:status 200
      :body (:uri request)
      }))
 
-
+(def spotify-oauth2-params
+  {:base-url "https://www.example.com"
+   :client-id (System/getenv "SPOTIFY_OAUTH2_CLIENT_ID")
+   :redirect-uri "https://www.example.com/callback"
+   :scope "do-one-thing do-another-thing"
+   :custom-query-params {:likes-cake "Yes"}
+   :client-secret (System/getenv "SPOTIFY_OAUTH2_CLIENT_SECRET")})
 
