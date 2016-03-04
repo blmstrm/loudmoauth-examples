13472a44dcf5290eab97acf0ffc8b59650ba4836
Kalle Blomström <kalle.blomstrom@gmail.com>
2016-03-05 00:16:29 +0100

    Gets interaction working.

--- src/loudmoauth_examples/core.clj
+++ src/loudmoauth_examples/core.clj
@@ -1,19 +1,23 @@
 (ns loudmoauth-examples.core
   (:require [loudmoauth.core :as lm]
-            [ring.adapter.jetty :as ringj]))
+            [ring.adapter.jetty :as ringj]
+            [ring.util.response :as ringr]
+            ))
 
 (defn handler [request]
+  (println request)
   (condp = (:uri request)
-     "/oauth2" (lm/parse-code request) 
+     "/oauth2" (lm/parse-code request)
+      "/interact"  (ringr/redirect (lm/user-interaction request))  
     {:status 200
      :body (:uri request)
      }))
 
 (def spotify-oauth2-params
-  {:base-url "https://www.example.com"
+  {:base-url "https://accounts.spotify.com"
    :client-id (System/getenv "SPOTIFY_OAUTH2_CLIENT_ID")
-   :redirect-uri "https://www.example.com/callback"
-   :scope "do-one-thing do-another-thing"
-   :custom-query-params {:likes-cake "Yes"}
+   :redirect-uri "http://localhost:3000/oauth2"
+   :scope "playlist-read-private user-follow-modify"
+   :custom-query-params {:show-dialog "true"}
    :client-secret (System/getenv "SPOTIFY_OAUTH2_CLIENT_SECRET")})
 
