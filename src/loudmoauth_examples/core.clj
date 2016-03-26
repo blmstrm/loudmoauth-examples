bbfe414100751b17719058822b4d0a9ec35adae7
Kalle Blomström <kalle.blomstrom@gmail.com>
2016-03-26 10:53:23 +0100

    Adds :provider key to oauth-param map

--- src/loudmoauth_examples/core.clj
+++ src/loudmoauth_examples/core.clj
@@ -2,13 +2,14 @@
   (:require [loudmoauth.core :as lm]
             [ring.adapter.jetty :as ringj]
             [ring.util.response :as ringr]
-            ))
+            [ring.middleware.params :as ringp]
+            [ring.middleware.keyword-params :as ringkp]))
 
 (defn handler [request]
   (println request)
   (condp = (:uri request)
      "/oauth2" (lm/parse-code request)
-      "/interact"  (ringr/redirect (lm/user-interaction request))  
+      "/interact"  (ringr/redirect (lm/user-interaction))  
     {:status 200
      :body (:uri request)
      }))
@@ -19,5 +20,11 @@
    :redirect-uri "http://localhost:3000/oauth2"
    :scope "playlist-read-private user-follow-modify"
    :custom-query-params {:show-dialog "true"}
-   :client-secret (System/getenv "SPOTIFY_OAUTH2_CLIENT_SECRET")})
+   :client-secret (System/getenv "SPOTIFY_OAUTH2_CLIENT_SECRET")
+   :provider :spotify
+   })
 
+(defn start-server
+  "Starts our test web server."
+  []
+ (future (ringj/run-jetty (ringp/wrap-params (ringkp/wrap-keyword-params handler))  {:port 3000})))
